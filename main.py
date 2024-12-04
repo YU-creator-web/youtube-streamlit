67975a43313f511ff1413cb9dda262d601194b56
YU <jesuis00@gmail.com>
2024-12-04 11:43:38 +0900

    Update main.py and requirements.txt

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-from PIL import Image
 import time
 
 st.title("Streamlit 入門")
@@ -30,15 +27,6 @@ expander2 = st.expander('問い合わせ2')
 expander2.write('問い合わせ2の回答')
 expander3 = st.expander('問い合わせ3')
 expander3.write('問い合わせ3の回答')
-# text = st.text_input('あなたの趣味を教えてください。')
-# condition = st.slider('あなたの今の調子は？', 0, 100, 50)
-
-# 'あなたの趣味：',text
-# 'コンディション：', condition
-
-# img = Image.open('sample.jpg')
-# st.image(img, caption='Kohei Imanishi',use_container_width=True)
-
 
 
 
